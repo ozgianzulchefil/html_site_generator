95b333cec8fc750f4a82004bdfb468c33295a02c
whitenight09 <ozzy@mail.com>
2026-01-10 14:08:52 +0200

    added additional tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -53,5 +53,27 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_textbold(self):
+        node = TextNode("THIS IS A BOLD TEXT NODE", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "THIS IS A BOLD TEXT NODE")
+
+    def test_textlink(self):
+        node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a text node")
+        self.assertEqual(html_node.props["href"], "https://www.boot.dev")
+
+    def test_textimage(self):
+        node = TextNode("This is a text node", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://www.boot.dev")
+        self.assertEqual(html_node.props["alt"], "This is a text node")
+
+
 if __name__ == "__main__":
     unittest.main()
